ce0b7471d02b6a7ed855c87271912f5bb119b7fb
PaulT1983 <pault83@gmail.com>
2024-08-10 17:07:26 +0300

    HW#44 Solution for even-odd messages

--- src/telran/multithreading/ProducerSender.java
+++ src/telran/multithreading/ProducerSender.java
@@ -9,21 +9,26 @@ public class ProducerSender extends Thread {
 	//two message boxes
 	//even messages should be put to even message box
 	//odd messages should be put to odd message box
-	private BlockingQueue<String> messageBox;
+	private BlockingQueue<String> oddMessageBox;
+	private BlockingQueue<String> evenMessageBox;
 	private int nMessages;
-	public ProducerSender(BlockingQueue<String> messageBox, int nMessages) {
-		this.messageBox = messageBox;
+	
+	public ProducerSender(BlockingQueue<String> oddMessageBox, 
+			BlockingQueue<String> evenMessageBox, int nMessages) {
+		this.oddMessageBox = oddMessageBox;
+		this.evenMessageBox = evenMessageBox;
 		this.nMessages = nMessages;
 	}
 	public void run() {
 		IntStream.rangeClosed(1, nMessages)
-		.mapToObj(i -> "message" + i).forEach(m -> {
-			try {
-				messageBox.put(m);
-			} catch (InterruptedException e) {
-				//no interrupt logics
-			}
-		});
+			.forEach(i -> {				
+					try {
+		                BlockingQueue<String> targetBox = (i % 2 != 0) ? oddMessageBox : evenMessageBox;
+		                targetBox.put("message" + i);
+					} catch (InterruptedException e) {
+						//no interrupt logics
+					}			
+			});
 	}
 	
 }
--- src/telran/multithreading/SenderReceiverAppl.java
+++ src/telran/multithreading/SenderReceiverAppl.java
@@ -5,59 +5,53 @@ import java.util.concurrent.*;
 import java.util.stream.IntStream;
 
 public class SenderReceiverAppl {
-//TODO for HW #44 (ConsumerReceiver should not be updated)
-	//Provide functionality of dispatching
-	//Even messages must be processed by receiver threads with even id
-	//Odd messages must be processed by receiver threads with odd id
-	//Hints two message boxes: one for even messages and other for odd messages
-	
-	
+	// for HW #44 (ConsumerReceiver should not be updated)
+	// Provide functionality of dispatching
+	// Even messages must be processed by receiver threads with even id
+	// Odd messages must be processed by receiver threads with odd id
+	// Hints two message boxes: one for even messages and other for odd messages
+
 	private static final int N_MESSAGES = 2000;
 	private static final int N_RECEIVERS = 10;
 
-	public static void main(String[] args) throws InterruptedException {
-		BlockingQueue<String> messageBox = new LinkedBlockingQueue<String>();
-		ProducerSender sender = startSender(messageBox, N_MESSAGES);
-		ConsumerReceiver[] receivers = startReceivers(messageBox, N_RECEIVERS);
+	public static void main(String[] args) throws InterruptedException {		
+		BlockingQueue<String> oddMessageBox = new LinkedBlockingQueue<String>();
+		BlockingQueue<String> evenMessageBox = new LinkedBlockingQueue<String>();
+		ProducerSender sender = startSender(oddMessageBox, evenMessageBox, N_MESSAGES);
+		ConsumerReceiver[] receivers = startReceivers(oddMessageBox, evenMessageBox, N_RECEIVERS);
 		sender.join();
 		stopReceivers(receivers);
 		displayResult();
-		
-		
-
 	}
 
 	private static void displayResult() {
-		System.out.printf("counter of processed messsages is %d\n",
-				ConsumerReceiver.getMessagesCounter());
-		
+		System.out.printf("counter of processed messsages is %d\n", ConsumerReceiver.getMessagesCounter());
 	}
 
 	private static void stopReceivers(ConsumerReceiver[] receivers) throws InterruptedException {
-		for(ConsumerReceiver receiver: receivers) {
+		for (ConsumerReceiver receiver : receivers) {
 			receiver.interrupt();
 			receiver.join();
 		}
-		
 	}
 
-	private static ConsumerReceiver[] startReceivers(BlockingQueue<String> messageBox,
-			int nReceivers) {
-		ConsumerReceiver[] receivers = 
-		IntStream.range(0, nReceivers).mapToObj(i -> {
-			ConsumerReceiver receiver = new ConsumerReceiver();
-			receiver.setMessageBox(messageBox);
+	private static ConsumerReceiver[] startReceivers(BlockingQueue<String> evenMessageBox,
+			BlockingQueue<String> oddMessageBox, int nReceivers) {
+		ConsumerReceiver[] receivers = IntStream.range(0, nReceivers).mapToObj(i -> {
+			ConsumerReceiver receiver = new ConsumerReceiver();					
+			// Even ID receivers consume even messages; odd ID receivers consume odd messages			
+			receiver.setMessageBox(receiver.threadId() % 2 != 0 ? evenMessageBox : oddMessageBox);
 			return receiver;
 		}).toArray(ConsumerReceiver[]::new);
-		Arrays.stream(receivers).forEach(ConsumerReceiver::start);
+		Arrays.stream(receivers).forEach(ConsumerReceiver::start);		
 		return receivers;
 	}
 
-	private static ProducerSender startSender(BlockingQueue<String> messageBox,
+	private static ProducerSender startSender(BlockingQueue<String> oddMessageBox, BlockingQueue<String> evenMessageBox,
 			int nMessages) {
-		ProducerSender sender = new ProducerSender(messageBox, nMessages);
+		ProducerSender sender = new ProducerSender(oddMessageBox, evenMessageBox, nMessages);
 		sender.start();
 		return sender;
 	}
-
+	
 }
